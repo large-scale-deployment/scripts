6a313ea2dc84e41a003e20b4783c941492cc1916
wxw-matt <matt.wxw.adelaide@gmail.com>
2022-04-01 20:50:42 +1030

    add installation of kubectl

--- install_minkube.sh
+++ install_minkube.sh
@@ -1,4 +1,9 @@
+# Install minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+# Install Kubectl
+/bin/bash -c "$(https://bit.ly/3gWAFoJ)"
+# Start minikube
 minikube delete
 minikube start
+
